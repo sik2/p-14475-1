6b19d644554edcbb8c7ae5ab4c6e64b396f92d6c
sik2 <bickwang123@gmail.com>
2025-08-01 14:20:43 +0900

    step1 r

---
+++ src/main/java/com/ll/Main.java
@@ -0,0 +1,9 @@
+package com.ll;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<Number> stack =  new Stack<>();
+    }
+}
---
+++ src/test/java/com/ll/CalcTest.java
@@ -0,0 +1,15 @@
+package com.ll;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CalcTest {
+    @Test
+    @DisplayName("1 + 1 == 2")
+    void t1() {
+        assertThat(Calc.run("1 + 1")).isEqualTo(2);
+    }
+}
